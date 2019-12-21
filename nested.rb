1b106d286343229216a9bfd7d85ab52918b0b9e2
Lydia B <lydiabaik@gmail.com>
2019-12-21 17:39:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,5 +127,5 @@ def adding_to_dennis
         }
      }
 	programmer_hash[:dennis_ritchie][:languages].push ("Assembly")
-	return programmer_hash[:dennis_ritchie][:languages]
+	return programmer_hash
 end
