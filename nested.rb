82a76e96b99512554859fcec46c527a1aa1ab535
Lydia B <lydiabaik@gmail.com>
2019-12-21 17:53:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,6 @@ def adding_matz
         }
      }
 		 :programmer_hash[:yukihiro_matsumoto] = {}
-		 :programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
 		 return programmer_hash
 end
 
